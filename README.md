cc7565d9def36d7e8ec415425e75d7ccacdc1c32
namitaChhantyal <chhantyalnamita111@gmail.com>
2025-09-24 22:25:34 -0400

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MedicalChatbot
+
+Project repo: https://github.com/namitaChhantyal/MedicalChatbot.git
+
+Create Virtual environment
+
+py -3.11 -m venv medibot
+
+medbot\Scripts\activate
+
+medibot\Scripts\activate
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Namita Chhantyal',
+    author_email='chhantyalnamita.01@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
